61ed7ac3121231e2a2f1852f680007cc7bfe8480
Dossman-thomas <tdossman.development@gmail.com>
2024-08-19 10:25:24 -0400

    simple dashboard component created

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService, User } from '../services/user.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  users: User[] = [];
+  totalUsers: number = 0;
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.users = this.userService.getUsers();
+    this.totalUsers = this.users.length; // Calculate total users
+  }
 }
